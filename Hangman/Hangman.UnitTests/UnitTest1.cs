18382862f75ff5fd166afb7b63e02124d1af2359
Brian Snyder <bsnyder@technology4retailers.com>
2016-12-02 09:29:35 -0600

    initial commit

---
+++ Hangman/Hangman.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSettingWord()
+        {
+            string word = "welcome";
+            Hangman hangman = new Hangman();
+            hangman.Word = word;
+            CollectionAssert.AreEqual(hangman.HiddenWord, word.ToCharArray());
+            Assert.AreEqual(hangman.Word, word);
+        }
+    }
+}
---
+++ Hangman/Hangman/Hangman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Hangman
+    {
+        private string _word;
+        private char[] _hiddenWord;
+
+        public string Word
+        {
+            set
+            {
+                _word = value;
+                _hiddenWord = value.ToCharArray();
+            }
+            get { return _word; }
+        }
+
+        public char[] HiddenWord
+        {
+            get { return _hiddenWord; }
+        }
+    }
+}
